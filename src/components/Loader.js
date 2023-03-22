00d553625a8916195099f20e5c2863378b153084
Sagar Sen <99211640+sagarsen123@users.noreply.github.com>
2023-03-22 16:54:16 +0530

    Search Removed

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 import loadingBall from "../images/loading_ball.gif"
 
-export class Loader extends Component {
-  render() {
+const Loader = () => {
+  
     return (
       <div className='container-xxl d-flex justify-content-center align-items-center'  style={{height:"40rem"}}><img src={loadingBall} alt="" /></div>
     )
-  }
+  
 }
 
 export default Loader
